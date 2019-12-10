e8d1faa3d15f344dfe4287bcc9221d15429d84c0
Clement Truillet <clem.truillet@gmail.com>
2019-12-10 18:46:11 +0100

    Create a bash script to know files in requete directory

---
+++ bin/readRequete.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+> index.dat #Vider le fichier index
+
+i=1
+for fich in `ls -1 ../requete/`
+do
+    if [[ ${fich: -4} == ".xml" || ${fich: -4} == ".wav" || ${fich: -4} == ".bin" || ${fich: -4} == ".bmp" || ${fich: -4} == ".jpg" ]]
+    then
+        echo "$i\- $fich" >> index.dat
+        let "i = i +1"
+    fi
+done
+
+
+
+exit 0

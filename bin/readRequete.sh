e037951253a140e155d639f928cd3fe4eeb8fb65
Clement Truillet <clem.truillet@gmail.com>
2019-12-12 08:14:45 +0100

    Remove numbers in generate index.dat file

--- bin/readRequete.sh
+++ bin/readRequete.sh
@@ -2,13 +2,11 @@
 
 > data/index.dat #Vider le fichier index
 
-i=1
 for fich in `ls -1 requete/`
 do
     if [[ ${fich: -4} == ".xml" || ${fich: -4} == ".wav" || ${fich: -4} == ".bin" || ${fich: -4} == ".bmp" || ${fich: -4} == ".jpg" ]]
     then
-        echo "$i\- $fich" >> data/index.dat
-        let "i = i +1"
+        echo $fich >> data/index.dat
     fi
 done
 

1661ad2c689b60dedf760a8bbb59c01f17f66522
Telkan <gamba.gael@orange.fr>
2019-12-18 18:38:40 +0100

    Creation de audio.c

---
+++ src/audio.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct audioDesc{
+	unsigned int size;
+	//TODO ajouter la fifo
+	//TODO ajouter l'identifiant du fichier
+}DescripteurAudio;
+
+
+
+//TODO : Lire un fichier et en faire un descripteur
+
+//TODO : Transformer un descripteur en string
+
+//TODO : Transformer un string en descripteur
+
+//TODO : Comparer 2 descripteurs
+

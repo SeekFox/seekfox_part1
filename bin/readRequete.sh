6e9e45ddf21167b3bdfa4d358f123a61853cc3d1
Clement Truillet <clem.truillet@gmail.com>
2019-12-18 14:55:45 +0100

    Recuperation des fichiers dans le dossier requete triés par extension

--- bin/readRequete.sh
+++ bin/readRequete.sh
@@ -2,7 +2,7 @@
 
 > data/index.dat #Vider le fichier index
 
-for fich in `ls -1 requete/`
+for fich in `ls -1 --sort=extension requete/`
 do
     if [[ ${fich: -4} == ".xml" || ${fich: -4} == ".wav" || ${fich: -4} == ".bin" || ${fich: -4} == ".bmp" || ${fich: -4} == ".jpg" ]]
     then

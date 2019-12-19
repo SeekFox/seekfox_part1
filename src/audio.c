3318d100371398a2bcf35b579e51a0e1aede153b
Gaël Gamba <gamba.gael@orange.fr>
2019-12-19 09:51:36 +0100

    Ajout lecture fichier bin

--- src/audio.c
+++ src/audio.c
@@ -2,18 +2,53 @@
 #include <stdlib.h>
 #include "../include/file.h"
 
+#define WAV_OFFSET 56
+
 typedef struct audioDesc{
 	unsigned int size;
-	File data;
-	//TODO ajouter l'identifiant du fichier
+	char* data;
+	char* fileID;
 }DescripteurAudio;
 
 
 
-//TODO : Lire un fichier et en faire un descripteur
-DescripteurAudio creerDescripteurAudio(FILE* p_file){
+
+	//////////////////////////////
+	//   CREER UN DESCRIPTEUR   //
+	//////////////////////////////
+
+
+int getAudioFileSize(FILE* p_file, int fileType){ //Calculer la taille d'un fichier audio, /!\ change le curseur de position, possible TODO a faire
+	unsigned int size;
+	double placeholder; //On est pas intéressé par les valeurs, faut juste une variable pour pouvoir lire
+
+	if(fileType == 1)	//Les .wav ont un en tete de 56 octets qui ne nous est pas utile ici
+		fseek(p_file, WAV_OFFSET, SEEK_SET);
+	else
+		fseek(p_file, 0, SEEK_SET);
+
+	do{
+		fread(&placeholder, sizeof(double), 1, p_file);
+		size++;
+	}while(!feof(p_file));
+
+	return size;
+}
+																						//Filetype : 2.bin, 1.wav, 0.txt
+DescripteurAudio creerDescripteurAudio(FILE* p_file, int tailleFenetre, int subdivisionFenetre, int fileType){	
+	
 	DescripteurAudio newDescripteur;
-	double whatsinthere = 0;
+	unsigned int fileSize;
+	double newFenetre[tailleFenetre];
+
+	if(fileType == 1)	//Les .wav ont un en tete de 56 octets qui ne nous est pas utile ici
+		fseek(p_file, WAV_OFFSET, SEEK_SET);
+	if(!feof(p_file)){
+		for(int i = 0; i < tailleFenetre; i++){
+			//newFenetre[i] = 
+		}
+	}
+
 	return newDescripteur;
 }
 
@@ -21,16 +56,16 @@ int main(){
 	FILE *p_file;
 	char temp[sizeof(double)];
 	double whatsinthere = 0;
-	p_file = fopen("../data/TEST_SON/jingle_fi.bin","rb");
 	
-	for (int i = 0; i < sizeof(double); ++i)
-	{
+	p_file = fopen("../data/TEST_SON/jingle_fi.bin","rb");
+	/*while(!feof(p_file)){
 		fread(&whatsinthere, sizeof(double), 1, p_file);
 		printf("%lf\n", whatsinthere);
-	}
-	
+	}*/
+
+	printf("Taille du fichier : %i",getAudioFileSize(p_file, 2));
 	fclose(p_file);
-	while(1);
+	return 0;
 }
 //TODO : Transformer un descripteur en string
 

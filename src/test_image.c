c82fa32152d035f8d1e9f8333b59f7bfa0403392
Oualid <oualid.el-abdaoui@univ-tlse3.fr>
2019-12-21 11:47:08 +0100

    quantification terminee

--- src/test_image.c
+++ src/test_image.c
@@ -1,15 +1,16 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<math.h>
 typedef int *bits;
 typedef int **matrice;
 
 int quantifier(int composante_rouge,int composante_vert,int composante_bleue,int n);
 
-/*int quantifier(int composante_rouge,int composante_vert,int composante_bleue,int n){
+int quantifier(int composante_rouge,int composante_vert,int composante_bleue,int n){
 	int dim = n*3;
 	bits b = malloc(dim*sizeof(int));
 	if(b == NULL)exit(1);
-	b[dim] =(int){0};
+	//b[dim] =(int){0};
 	int rouge[8],vert[8],bleue[8];
 	int i=0;
 	while(composante_rouge > 0 || composante_vert > 0 || composante_bleue > 0){
@@ -32,7 +33,7 @@ int quantifier(int composante_rouge,int composante_vert,int composante_bleue,int
 			/*for(i=0;i<9;i++){
 				if(i<3)b[i] = rouge[7-i];
 				else if(i<5)for(int j=7;j>4;j--)b[i] = vert[j];
-				else for(int j=7;j>4;j--)b[3] = vert[7];
+				else for(int j=7;j>4;j--)b[3] = vert[7];*/
 				
 		b[0] = rouge[7];
 		b[1] = rouge[6];
@@ -45,16 +46,19 @@ int quantifier(int composante_rouge,int composante_vert,int composante_bleue,int
 		b[8] = bleue[5];
 		}
 		
-	return ;
-}*/
+	int resultat = 0;
+	for(int j=0;j<dim;j++){resultat = resultat + b[j]*pow(2,dim-1-j);}
+	
+	return resultat;
+}
 
-void AllouerMemoire(int **tab,int taille){
+/*void AllouerMemoire(int **tab,int taille){
 	*tab = (int*)calloc(taille,sizeof(int)*taille);
-	if(tab == NULL) exit(EXIT_FAILURE);
+	if(tab == NULL) exit(EXIT_FAILURE);*/
 }
 int main(void){
 /*-----------------------------------test de la fonction quantifier-------------------*/
-	/*int n,cr,cv,cb;
+	int n,cr,cv,cb,resultat;
 	
 	printf("sasir le nombre de bits sur quoi quantifier\n");
 	scanf("%d",&n);
@@ -62,19 +66,19 @@ int main(void){
 	printf("saisir les composantes\n");
 	scanf("%d%d%d",&cr,&cb,&cv);
 	
-	bits b = malloc(n*3*sizeof(int));
-	if(b == NULL)exit(EXIT_FAILURE);
-	b = quantifier(cr,cb,cv,n);
-	for(int i=0;i<n*3;i++)printf("%d ",b[i]);
-	printf("\n");
-	*/
+	//bits b = malloc(n*3*sizeof(int));
+	//if(b == NULL)exit(EXIT_FAILURE);
+	resultat = quantifier(cr,cb,cv,n);
+	//for(int i=0;i<n*3;i++)printf("%d ",b[i]);
+	printf("\n%d\n",resultat);
+	
 /*------------------------------------------------------------------------------------*/
-int *tab = NULL;
+/*int *tab = NULL;
 AllouerMemoire(&tab,5);
 for(int i=0;i<5;i++){
 	tab[i] = i;
 	printf("%d",tab[i]);
-	}
+	}*/
 /*------------------------------------------------------------------------------------*/
 /*	int NbLignes,NbColonnes,NombreComposantes;
 	char nomfichier[10];

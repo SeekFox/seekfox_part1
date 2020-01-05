3d41621b2ab771fd1cce04038823b165b2436a03
Clement Truillet <clem.truillet@gmail.com>
2020-01-05 13:27:19 +0100

    Modification QuantificationRGB

--- src/test_image.c
+++ src/test_image.c
@@ -17,14 +17,14 @@ s'il y a des parties incompréhensibles envoyez moi un message
 Merci ^_^
 */
 
-#include<stdio.h>
-#include<stdlib.h>
-#include<math.h>
-#include<string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
 /*-------------------------------------------------------------------------------------*/
 
-typedef int *bits;
-typedef int **matrice;
+typedef int * bits;
+typedef int ** matrice;
 
 /*typedef struct Descripteur{
 	char identifiant[5];
@@ -32,7 +32,7 @@ typedef int **matrice;
 }Descripteur;*/
 
 int quantifierRGB(int composante_rouge,int composante_vert,int composante_bleue,int n);
-//int taille_max =(int)pow(2,n*NombreComposantes);
+//int taille_max =(int)pow(2,n*nbComposantes);
 
 
 /*-------------------------------------------------------------------------------------*/
@@ -42,13 +42,16 @@ int quantifierRGB(int composante_rouge,int composante_vert,int composante_bleue,
 	// cette fonction sert à quantifier un pixel à partir de 3 composantes rouge verte est bleue cf : cahier de charges annexe page 15-16
 
 	int dim = n*3; // la dimension du tableau de bits
+	int resultat = 0;
 
 	bits b = malloc(dim*sizeof(int)); // allocation dynamique du tableau de bits
 	if(b == NULL)exit(1);
 
-	int rouge[8] = {0},vert[8] = {0},bleue[8] = {0}; 
-	// les tableau cotenants les résultats des divisions successives des trois composantes -> 8 éléments puique la valeur maximale d'une composante est 255 = 2^8-1
-	
+	int rouge[8];
+	int vert[8];
+	int bleue[8];
+	// les tableau contenants les résultats des divisions successives des trois composantes -> 8 éléments puique la valeur maximale d'une composante est 255 = 2^8-1
+
 	for(int i=0;i<8;i++){ // boucle pour la conversion des composantes en décimales en binaire
 
 		rouge[i] = composante_rouge%2;
@@ -59,39 +62,13 @@ int quantifierRGB(int composante_rouge,int composante_vert,int composante_bleue,
 
 		bleue[i] = composante_bleue%2;
 		composante_bleue = composante_bleue/2;
-
 	}
-		
-	if(dim == 6){	// cas de la quantification sur 2 bits -> tableau de 6 bits
-		for(int i = 0;i<dim;i++){
 
-			if(i<2)b[i] = rouge[7-i];
-
-			else if(i<4)b[i] = vert[9-i];
-
-			else b[i] = bleue[11-i];
-		}
+	for(int i=0; i<n; i++){
+		resultat+=rouge[8-i]*(int)pow((int)2,(int)(3*n-1-i));
+		resultat+=vert[8-i] *(int)pow((int)2,(int)(2*n-1-i));
+		resultat+=bleue[8-i]*(int)pow((int)2,(int)(1*n-1-i));
 	}
-	else if(dim == 9){ // cas de la quantification sur 3 bits -> tableau de 3 bits
-		for(int i=0;i<9;i++){
-
-			if(i<3)b[i] = rouge[7-i];
-
-			else if(i<6)b[i] = vert[10-i];
-
-			else b[i] = bleue[13-i];}
-	}
-		
-	int resultat = 0,a = 0;
-	for(int j=0;j<dim;j++){// boucle pour le conversion binaire au décimal et récupération du résultat 
-
-		a =(int)pow(2,dim-1-j);
-
-		resultat = resultat + (b[j]*a);
-
-		}
-
-	free(b);
 
 return resultat;
 }
@@ -147,7 +124,7 @@ void remplirMatrice(FILE **f,matrice M,int lignes,int colonnes){
 
 	for(int i=0;i<lignes;i++){
 		for(int j=0;j<colonnes;j++){
-			fscanf(*f,"%d",&M[i][j]); 		
+			fscanf(*f,"%d",&M[i][j]);
 		}
 	}
 }
@@ -216,17 +193,14 @@ int main(void){
 
 /*----------------------------partie commande unix pour lister les noms des fichiers------------------------------*/
 
-	int NbLignes = 0,NbColonnes = 0,NombreComposantes = 0,n = 0,nombre_fichiers = 0;
-	char CHEMIN[100] = "../data/";
-	char commande[1000];
+	int nbLignes = 0,nbColonnes = 0,nbComposantes = 0,n = 0,nbFichiers = 0;
+	char path[16] = "../data/";
+	char commande[64] = "";
 	
-
-	strcpy(commande,"ls ");
-	strcat(commande, CHEMIN);
-	strcat(commande, "TEST_IMAGES > ../data/liste_des_images");
+	sprintf(commande,"%s %s%s","ls",path,"TEST_IMAGES > ../data/liste_des_images");
 
 	printf("execution de %s\n",commande);
-	system(commande); // commande qui sert à mettre la liste des noms des fichiers(images) .txt à indexer
+	//system(commande); // commande qui sert à mettre la liste des noms des fichiers(images) .txt à indexer
 
 	// la commande est ls ../data/TEST_IMAGES > ../data/liste_des_images
 
@@ -234,22 +208,22 @@ int main(void){
 
 	/*------------------------------ouverture des fichiers à indexer--------------------------------------------------*/
 
-	FILE *entree;
-	FILE *lecteur_fichier;
-	FILE *compteur_fichiers;
+	FILE * entree;
+	FILE * lecteur_fichier;
+	FILE * compteur_fichiers;
 
 
 	lecteur_fichier = fopen("../data/liste_des_images","r");
 	//ouverture du fichier contenant la liste des images.txt
 
-	system("wc -l ../data/liste_des_images > ../data/nombre_fichiers");
+	system("wc -l ../data/liste_des_images > ../data/nbFichiers");
 	//commande unix wc -l pour compter le nombre de lignes dans la liste des images 
 
-	compteur_fichiers = fopen("../data/nombre_fichiers","r");
-	fscanf(compteur_fichiers," %d",&nombre_fichiers);
+	compteur_fichiers = fopen("../data/nbFichiers","r");
+	fscanf(compteur_fichiers," %d",&nbFichiers);
 	//lecture du nombre de fichiers
 
-	//printf("%d\n",nombre_fichiers); // affichage du nombre de fichiers
+	//printf("%d\n",nbFichiers); // affichage du nombre de fichiers
 
 	fclose(compteur_fichiers);
 
@@ -275,127 +249,128 @@ int main(void){
 	}
 /*-------------------------------------------------------------------------------------*/}
 	
-	for(int i=1;i<=nombre_fichiers;i++){ // boucle for qui dépend du nombre de fichiers
-	char titre_fichier[6];
-	fscanf(lecteur_fichier,"%s",titre_fichier);//lecture des titres des fichiers
+	for(int i=1;i<=nbFichiers;i++){ // boucle for qui dépend du nombre de fichiers
+		char titre_fichier[6];
+		fscanf(lecteur_fichier,"%s",titre_fichier);//lecture des titres des fichiers
 
-	/*	do
-		{
-			fscanf(lecteur_fichier,"%s",titre_fichier);
-			if(titre_fichier[5] != 't')nombre_fichiers--;
-		} while(titre_fichier[5] != 't');*/ //verification du format du fichier .txt ou pas
-	
-		//printf("titre fichier lu\n");
-
-		//printf("\n%s\n",titre_fichier);
-
-		char CHEMIN2[100]="../data/TEST_IMAGES/"; // chemin d'ouverture des fichiers
-		strcat(CHEMIN2,titre_fichier);
-
-		printf("\n%s\n",CHEMIN2);
-
-
-		entree = fopen(CHEMIN2,"r"); // ouverture des fichiers .txt en mode lecture
+		/*	do
+			{
+				fscanf(lecteur_fichier,"%s",titre_fichier);
+				if(titre_fichier[5] != 't')nbFichiers--;
+			} while(titre_fichier[5] != 't');*/ //verification du format du fichier .txt ou pas
 		
-		fscanf(entree," %d%d%d",&NbLignes,&NbColonnes,&NombreComposantes); // lecture de la première lignes dimension de l'image et les composantes
-		//printf("%d %d %d\n",NbLignes,NbColonnes,NombreComposantes);
+			//printf("titre fichier lu\n");
 
-		int taille_max =(int)pow(2,n*NombreComposantes);
-		
-		int *Histogramme = NULL;
-		Histogramme = malloc(taille_max*sizeof(int));
-		if(Histogramme == NULL)exit (EXIT_FAILURE);
+			//printf("\n%s\n",titre_fichier);
 
-		Histogramme[taille_max] =(int){0};
-		// allocation dynamique et initialisation de l'histogramme
+			char CHEMIN2[100]="../data/TEST_IMAGES/"; // chemin d'ouverture des fichiers
+			strcat(CHEMIN2,titre_fichier);
 
-		/*l'histogramme a une dimention de 2^NombreComposantes*nombre de bits sur quoi quantifier donc
-		RBG(trois composante) sur 2 bits Histogramme de 64 valeurs
-		RGB(trois composante) sur 3 bits Histogramme de 512 valeurs
-		NB(une composante) sur 2 bits Histogramme de 4 valeurs
-		NB(une composante) sur 3 bits Histogramme de 8 valeurs
-		cf : cahier de charges page 17
-		*/
-
-		if(NombreComposantes == 3){
-
-			matrice Rouge = NULL,Verte = NULL,Bleue = NULL,ImageRGB = NULL;
-			Rouge = allouerMemoire(Rouge,NbLignes,NbColonnes);
-			Verte = allouerMemoire(Verte,NbLignes,NbColonnes);
-			Bleue = allouerMemoire(Bleue,NbLignes,NbColonnes);
-			ImageRGB = allouerMemoire(ImageRGB,NbLignes,NbColonnes);
-			//allocation dynamique des matrices représentant les composantes de l'image
-
-			//printf("allouer memoire reussi\n");
+			printf("\n%s\n",CHEMIN2);
 
 
-			remplirMatrice(&entree,Rouge,NbLignes,NbColonnes);
-			remplirMatrice(&entree,Verte,NbLignes,NbColonnes);
-			remplirMatrice(&entree,Bleue,NbLignes,NbColonnes);
-			//lecture du contenu de l'image et remplissage des matrices
+			entree = fopen(CHEMIN2,"r"); // ouverture des fichiers .txt en mode lecture
+			
+			fscanf(entree," %d%d%d",&nbLignes,&nbColonnes,&nbComposantes); // lecture de la première lignes dimension de l'image et les composantes
+			printf("%d %d %d\n",nbLignes,nbColonnes,nbComposantes);
 
-			//printf("matrices remplies reussi\n");
+			int taille_max =(int)pow(2,n*nbComposantes);
+			printf("%d\n",taille_max);
+			
+			int * Histogramme = NULL;
+			Histogramme = malloc(taille_max*sizeof(int));
+			if(Histogramme == NULL) exit (EXIT_FAILURE);
 
-			realiserHistogrammeRGB(ImageRGB,Rouge,Verte,Bleue,NbLignes,NbColonnes,n,Histogramme,taille_max);
-			//realisation de l'histogramme avec passage en parametre du tableau Histogramme
+			Histogramme[taille_max] =(int){0};
+			// allocation dynamique et initialisation de l'histogramme
 
-			//printf("histogramme reussi\n");
+			/*l'histogramme a une dimention de 2^nbComposantes*nombre de bits sur quoi quantifier donc
+			RBG(trois composante) sur 2 bits Histogramme de 64 valeurs
+			RGB(trois composante) sur 3 bits Histogramme de 512 valeurs
+			NB(une composante) sur 2 bits Histogramme de 4 valeurs
+			NB(une composante) sur 3 bits Histogramme de 8 valeurs
+			cf : cahier de charges page 17
+			*/
 
-			Rouge = libererMemoire(Rouge,NbColonnes);
-			Verte = libererMemoire(Verte,NbColonnes);
-			Bleue = libererMemoire(Bleue,NbColonnes);
-			ImageRGB = libererMemoire(ImageRGB,NbColonnes);
-			//libération de la mémoire
+			if(nbComposantes == 3){
 
+				matrice Rouge = NULL,Verte = NULL,Bleue = NULL,ImageRGB = NULL;
+				Rouge = allouerMemoire(Rouge,nbLignes,nbColonnes);
+				Verte = allouerMemoire(Verte,nbLignes,nbColonnes);
+				Bleue = allouerMemoire(Bleue,nbLignes,nbColonnes);
+				ImageRGB = allouerMemoire(ImageRGB,nbLignes,nbColonnes);
+				//allocation dynamique des matrices représentant les composantes de l'image
+
+				//printf("allouer memoire reussi\n");
+
+
+				remplirMatrice(&entree,Rouge,nbLignes,nbColonnes);
+				remplirMatrice(&entree,Verte,nbLignes,nbColonnes);
+				remplirMatrice(&entree,Bleue,nbLignes,nbColonnes);
+				//lecture du contenu de l'image et remplissage des matrices
+
+				//printf("matrices remplies reussi\n");
+
+				realiserHistogrammeRGB(ImageRGB,Rouge,Verte,Bleue,nbLignes,nbColonnes,n,Histogramme,taille_max);
+				//realisation de l'histogramme avec passage en parametre du tableau Histogramme
+
+				//printf("histogramme reussi\n");
+
+				Rouge = libererMemoire(Rouge,nbColonnes);
+				Verte = libererMemoire(Verte,nbColonnes);
+				Bleue = libererMemoire(Bleue,nbColonnes);
+				ImageRGB = libererMemoire(ImageRGB,nbColonnes);
+				//libération de la mémoire
+
+			}
+			else if(nbComposantes == 1){
+				//printf("choix effectue\n");
+				matrice Noire = NULL,ImageNB = NULL;
+				//allocation dynamique de la matrice représentant la composante noire et l'image pour réaliser l'histogramme
+
+				Noire = allouerMemoire(Noire,nbLignes,nbColonnes);
+				ImageNB = allouerMemoire(ImageNB,nbLignes,nbColonnes);
+				//	printf("allocation memoire reussie\n");
+				//allocation dynamique de la matrice noire représentant l'image
+
+				remplirMatrice(&entree,Noire,nbLignes,nbColonnes);
+				//printf("matrices remplies reussi\n");
+				//lecture du contenu de l'image et remplissage de la matrice
+
+				realiserHistogrammeNB(ImageNB,Noire,nbLignes,nbColonnes,n,Histogramme,taille_max);
+				//realisation de l'histogramme avec passage en parametre du tableau Histogramme
+
+				Noire = libererMemoire(Noire,nbLignes);
+				ImageNB = libererMemoire(ImageNB,nbLignes);
+				//libération de la mémoire
+
+			}
+			else printf("Image non prise en charge \n");
+
+		/*fprintf(fichier_descripteur,"[ #id%d ]",i);
+
+		for(int j=0;j<taille_max;j++){
+			fprintf(fichier_descripteur,"%d ",Histogramme[j]);
+			Histogramme[j] = (int)0;
 		}
-		else if(NombreComposantes == 1){
-			//printf("choix effectue\n");
-			matrice Noire = NULL,ImageNB = NULL;
-			//allocation dynamique de la matrice représentant la composante noire et l'image pour réaliser l'histogramme
+		fprintf(fichier_descripteur,"\n");*/ // ecriture dans le fichier base_descripteur_image
 
-			Noire = allouerMemoire(Noire,NbLignes,NbColonnes);
-			ImageNB = allouerMemoire(ImageNB,NbLignes,NbColonnes);
-			//	printf("allocation memoire reussie\n");
-			//allocation dynamique de la matrice noire représentant l'image
+		int somme = 0;
 
-			remplirMatrice(&entree,Noire,NbLignes,NbColonnes);
-			//printf("matrices remplies reussi\n");
-			//lecture du contenu de l'image et remplissage de la matrice
+		printf("[ #id%d ]",i);
 
-			realiserHistogrammeNB(ImageNB,Noire,NbLignes,NbColonnes,n,Histogramme,taille_max);
-			//realisation de l'histogramme avec passage en parametre du tableau Histogramme
-
-			Noire = libererMemoire(Noire,NbLignes);
-			ImageNB = libererMemoire(ImageNB,NbLignes);
-			//libération de la mémoire
-
+		for(int j=0;j<taille_max;j++){
+			printf("%d ",Histogramme[j]);
+			somme = somme + Histogramme[j];
+			Histogramme[j] = 0;
 		}
-		else printf("Image non prise en charge \n");
 
-	/*fprintf(fichier_descripteur,"[ #id%d ]",i);
+		printf("\n somme : %d\n",somme);
+		somme = 0;
+		//affichage des descripteurs avec la somme des nombres de pixels pour vérification du résultat
 
-	for(int j=0;j<taille_max;j++){
-		fprintf(fichier_descripteur,"%d ",Histogramme[j]);
-		Histogramme[j] = (int)0;
+		free(Histogramme);
 	}
-	fprintf(fichier_descripteur,"\n");*/ // ecriture dans le fichier base_descripteur_image
-
-	int somme = 0;
-
-	printf("[ #id%d ]",i);
-
-	for(int j=0;j<taille_max;j++){
-		printf("%d ",Histogramme[j]);
-		somme = somme + Histogramme[j];
-		Histogramme[j] = 0;
-	}
-
-	printf("\n somme : %d\n",somme);
-	somme = 0;
-	//affichage des descripteurs avec la somme des nombres de pixels pour vérification du résultat
-
-	free(Histogramme);
-}
 	
 	//fclose(fichier_descripteur);
 	fclose(lecteur_fichier);

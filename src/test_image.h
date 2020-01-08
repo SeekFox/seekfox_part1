76f7c3917f08f9ff2e72ceaf8e22f051a4204f97
Oualid <oualid.el-abdaoui@univ-tlse3.fr>
2020-01-09 00:37:30 +0100

    commit

--- src/test_image.h
+++ src/test_image.h
@@ -1,7 +1,7 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<math.h>
-#include<string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
 
 typedef int * bits;
 typedef int ** matrice;
@@ -12,5 +12,7 @@ int quantifierNB(int ComposanteNoire,int n);
 matrice allouerMemoire(matrice tab,int lignes,int colonnes);
 matrice libererMemoire(matrice m,int colonnes);
 
+
 void remplirMatrice(FILE **f,matrice M,int lignes,int colonnes);
 void realiserHistogrammeRGB(matrice Image,matrice Rouge,matrice Verte,matrice Bleue,int lignes,int colonnes,int n,int Histogramme[],int taille_max);
+void realiserHistogrammeNB(matrice Image,matrice noire,int lignes,int colonnes,int n,int *Histogramme,int taille_max);
